441cf6e0125b72182e8ecdf6f0721c874818eab0
WGCAT <seed9878@naver.com>
2023-12-26 00:50:52 +0900

    rest_api

---
+++ web1/go.mod
@@ -0,0 +1,10 @@
+module main
+
+go 1.21.5
+
+require github.com/tuckersGo/goWeb v0.0.0-20200531032145-28d77cfd0a29
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+)
---
+++ web1/myapp/app_test.go
@@ -0,0 +1,20 @@
+package myapp
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndex(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.Code)
+}
