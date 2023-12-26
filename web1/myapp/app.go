565f32314de5065245ad6a17ea2f1e11de25bae1
WGCAT <seed9878@naver.com>
2023-12-27 02:59:27 +0900

    11

--- web1/myapp/app.go
+++ web1/myapp/app.go
@@ -1,11 +1,21 @@
 package myapp
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type User struct { //제이슨이 읽을 수 있는 유저 스트럭트를 만듦
+	ID        int       `json:"ID"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
@@ -14,8 +24,33 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Get UserInfo by /users/{id}")
 }
 func getUserInfoHandler(w http.ResponseWriter, r *http.Request) { //고정 89가 아니라 아이디를 나타내줘야하므로 mux.Vars사용한다
-	vars := mux.Vars(r) //알아서 테스트 패싱해주ㅠㅁ
-	fmt.Fprint(w, "User Id:", vars["id"])
+	user := new(User)
+	user.ID = 2
+	user.FirstName = "sujin"
+	user.LastName = "lee"
+	user.Email = "seed9878@gmail.com"
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+}
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) { //실제 유저를 생성하는 코드를 만들어야하는데 클라이언트가 유저정보를 제이슨으로 보냈음
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	// Created User
+	user.ID = 2
+	user.CreatedAt = time.Now()
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
 }
 
 // NewHandler make a new myapp handler
@@ -23,7 +58,8 @@ func NewHandler() http.Handler {
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET") //GET메소드일때 이 usersHandler가 불려라 정함
+	mux.HandleFunc("/users", CreateUserHandler).Methods("POST")
 	// mux.HandleFunc("/users/89", getUserInfo89Handler) 89가 아니라 아이디를 나타내는 {id:[0-9]+}문법으로 (고릴라)
 	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler)
 
--- web1/myapp/app_test.go
+++ web1/myapp/app_test.go
@@ -1,9 +1,12 @@
 package myapp
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,6 +48,38 @@ func TestGetUserInfo(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, resp.StatusCode)
 	data, _ := ioutil.ReadAll(resp.Body)
-	assert.Contains(string(data), "User Id:89") //89를 포함해야한다
+	assert.Contains(string(data), "No User Id:89") //89를 포함해야한다
 	//사실 정해진 아이디가 아닌 유저아이디가 뒤에 붙었을때 맞게 와야함 URL.Path를 가지고
+
+	resp, err := http.Get(ts.URL + "/users/56")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "No User Id:56")
+}
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Post(ts.URL+"/users", "application/json", //포스트로 보냈고
+		strings.NewReader(`{"first_name" : "sujin", "last_name":"lee", "email":"seed9878@gmail.com"}`)) // 유저데이터 보냈고
+	assert.NoError(err)                               //에러가 없어야한다
+	assert.Equal(http.StatusCreated, resp.StatusCode) // 만들어진거 확인했고
+	//읽어서 만들어진거 확인해야지
+	user := new(User)
+	err := json.NewDecoder(resp.Body).Decode(user) //만약에 서버가 보낸 제이슨 코드에 문제가
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID) //유저의 아이디가 0이 아니다
+
+	id := user.ID //아이디를 가지고 다시 Get을 해본다
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.NotEqual(http.StatusOK, resp.StatusCode)
+	user2 := new(User)
+	err := json.NewDecoder(resp.Body).Decode(user2)
+	assert.NoError(err)
+	assert.NotEqual(user.ID, user2.ID)
+	assert.NotEqual(user.FirstName, user2.FirstName)
 }
